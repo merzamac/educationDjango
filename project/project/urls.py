c5afc1c68cb5986322af986305397b3e347cd39d
merzamac <mmerza@uc.edu.ve>
2025-03-02 12:10:50 -0400

    solo para conocer la estructura

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+def saludar(self):
+    print("Hola mundo")
+    return True
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/',saludar)
 ]
